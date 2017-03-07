847877288989c2b8470db571ebede7f8282a8a41
mikemyzhao(赵梦屹) <mikemyzhao@tencent.com>
2017-03-07 09:07:05 +0800

    添加问题要求描述

--- src/com/mikemyzhao/stackqueue/AnimalQueue.java
+++ src/com/mikemyzhao/stackqueue/AnimalQueue.java
@@ -1,5 +1,16 @@
 package com.mikemyzhao.stackqueue;
 
+/*
+ * 有家动物收容所只收容狗和猫，且严格遵守“先进先出”的原则。
+ * 在收养该收容所的动物时，
+ * 收养人只能收养所有动物中“最老”（根据进入收容所的时间长短）的动物，
+ * 或者可以挑选猫或者狗（同时必须收养此类动物中“最老”的）。
+ * 换言之，收养人不能自由挑选想收养的对象。
+ * 请创建适用于这个系统的数据结构，实现各种操作方法，
+ * 比如enqueue、dequeueAny、dequeueDog和dequeueCat等。
+ * 允许使用Java内置的LinkedList数据结构。
+ */
+//相关的类Animal、CatDog
 import java.util.LinkedList;
 
 public class AnimalQueue {
